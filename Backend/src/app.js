5041cf61824313606485827343833fab9b93a31b
420pawan <pawan_kumar.ug23@nsut.ac.in>
2026-07-09 23:37:07 +0530

    Fix CORS origins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,37 @@
-const cors = require("cors");
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ai-interview-assistant-git-main-pawan11.vercel.app",
+    "https://ai-interview-assistant-pawan11.vercel.app"
+];
 
 app.use(cors({
-  origin: true,
-  credentials: true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error(`Origin ${origin} not allowed by CORS`));
+        }
+    },
+    credentials: true,
 }));
+
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes")
+const interviewRouter = require("./routes/interview.routes")
+
+
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
+
+
+
+module.exports = app
