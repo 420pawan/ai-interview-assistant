e70aadaf69cd4fcd2eca71bc5addb11cf427b2c2
420pawan <pawan_kumar.ug23@nsut.ac.in>
2026-07-09 18:33:10 +0530

    Initial commit

---
+++ Backend/src/app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ai-interview-assistant-pawan11.vercel.app"
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
+
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes")
+const interviewRouter = require("./routes/interview.routes")
+
+
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
+
+
+
+module.exports = app
